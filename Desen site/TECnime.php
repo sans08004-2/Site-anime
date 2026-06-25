e19d43650ad8bfa35751302e614424731f7b2fae
sans08004-2 <00001123911654sp@al.educacao.sp.gov.br>
2026-06-25 15:33:25 -0300

    Update and rename TECnime.html to TECnime.php
    mudei pra php mas ainda tenho mudanças a fazer tipo fazer com que o login mode pra seu nome qnado vc estiver logado

---
+++ Desen site/TECnime.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TECnime</title>
+    <link rel="stylesheet" href="TECnime.css">
+</head>
+<body>
+    <header>
+        
+        <nav>
+            <div class="Logo"><a href="TECnime.html">Logo</a></div>
+            <ul class="Pesquisa">
+
+                <input type="text" placeholder="Pesquisa">
+            <button type="submit">
+            <img src="https://cdn-icons-png.flaticon.com/512/622/622669.png" alt="Pesquisar">
+            </button>
+            </ul>
+            <ul class="Menu">
+                <li><a href="login.html">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+<div class="menulat">
+    <aside class="menulat2">
+        <ul>
+            <li><a href="TECnime.html">Home</a></li>
+            <li><a href="lojinha.html">Loja</a></li>
+            <li><a href="contato.html">Contato</a></li>
+            </ul>
+    </aside>
+
+    <div class="carrossel">
+        <div class="carrossell">
+            <div class="carrosselll">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYY3_ABG_0BppTfB_k9jdMCyZ0nTV-_MJeXg&s" alt="1">
+            </div>
+            <div class="carrosselll">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYY3_ABG_0BppTfB_k9jdMCyZ0nTV-_MJeXg&s" alt="2">
+            </div>
+            <div class="carrosselll">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYY3_ABG_0BppTfB_k9jdMCyZ0nTV-_MJeXg&s" alt="3">
+            </div>
+            <div class="carrosselll">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYY3_ABG_0BppTfB_k9jdMCyZ0nTV-_MJeXg&s" alt="4">
+            </div>
+        </div>
+        <div class="botoes">
+  <button id="esquerda">&#10094;</button>
+  <button id="direita">&#10095;</button>
+        </div>
+      </div>
+      </div>
+<div class="novidades">
+<H1>TECnime novidades</H1>
+<h3>Confira as últimas novidades do TECnime!</h3>
+<p>lore ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nunc nisl eget nunc.</p>
+</div>
+<script src="TECnime.js"></script>
+
+<div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
+</body>
+</html>
